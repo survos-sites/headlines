75e24ead5cbaf8267673a1105a0932efc72c7f03
Tac Tacelosky <tacman@gmail.com>
2024-07-02 08:09:02 -0400

    remove webfactory/polyglot-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,7 +16,6 @@ return [
     Survos\LibreTranslateBundle\SurvosLibreTranslateBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Webfactory\Bundle\PolyglotBundle\WebfactoryPolyglotBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     Survos\Bundle\MakerBundle\SurvosMakerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
